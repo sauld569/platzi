a195356aaff3e14b8945f6d319c211a85d2afb2a
Saul Daniel <sorozcocuevas@gmail.com>
2020-01-23 09:50:03 -0800

    Termina la clase 15

--- clase15/dibujo.js
+++ clase15/dibujo.js
@@ -1,3 +1,15 @@
+var d=document.getElementById("dibujo");
+var lienzo=d.getContext("2d");
+
+var y=0;
+var x=0;
+var a=0;
+var b=300;
+  do{
+  dibujarLineas("red", x,y,a,b);
+  y=y+10;
+  a=a+10;
+}while (a<300);
 function dibujarLineas(color, xInicial, yInicial, xFinal, yFinal){
   lienzo.beginPath();
   lienzo.strokeStyle=color;
@@ -6,6 +18,3 @@ function dibujarLineas(color, xInicial, yInicial, xFinal, yFinal){
   lienzo.stroke();
   lienzo.closePath();
 }
-
-var d=document.getElementById("dibujo");
-var lienzo=d.getContext("2d");
